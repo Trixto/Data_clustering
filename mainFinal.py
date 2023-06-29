f479dae56fb645e3699808ff3961e3bdb2df5937
Trixto <51496670+Trixto@users.noreply.github.com>
2023-06-29 12:00:39 +0200

    Update mainFinal.py

--- mainFinal.py
+++ mainFinal.py
@@ -2,137 +2,74 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
+import umap.umap_ as umap
 
 # Specify the paths to the data directories
 Cesty = [
-    'D:/Brno_clustering/Spolu_radiologovia/spolu_nativ/',
-    'D:/Brno_clustering/Spolu_radiologovia/spolu_faza1/',
-    'D:/Brno_clustering/Spolu_radiologovia/spolu_faza2/',
-    'D:/Brno_clustering/Spolu_radiologovia/spolu_faza3/',
-    'D:/Brno_clustering/Spolu_radiologovia/spolu_tMIP/',
-    'D:/Brno_clustering/Spolu_vsetci/spolu_nativ/',
-    'D:/Brno_clustering/Spolu_vsetci/spolu_faza1/',
-    'D:/Brno_clustering/Spolu_vsetci/spolu_faza2/',
-    'D:/Brno_clustering/Spolu_vsetci/spolu_faza3/',
-    'D:/Brno_clustering/Spolu_vsetci/spolu_tMIP/'
+    'D:/Brno_clustering/Vox_based/spolu_nativ/',
+    'D:/Brno_clustering/Vox_based/spolu_faza1/',
+    'D:/Brno_clustering/Vox_based/spolu_faza2/',
+    'D:/Brno_clustering/Vox_based/spolu_faza3/',
+    'D:/Brno_clustering/Vox_based/spolu_tMIP/',
 ]
 
-# Specify the names of the data files
 Data = [
-    'Data_radiologovia_nativ_nativ',
-    'Data_radiologovia_nativ_faza1',
-    'Data_radiologovia_nativ_faza2',
-    'Data_radiologovia_nativ_faza3',
-    'Data_radiologovia_nativ_tMIP',
-    'Data_vsetci_nativ',
-    'Data_vsetci_faza1',
-    'Data_vsetci_faza2',
-    'Data_vsetci_faza3',
-    'Data_vsetci_tMIP'
+    'Data_Vox_based_nativ',
+    'Data_Vox_based_faza1',
+    'Data_Vox_based_faza2',
+    'Data_Vox_based_faza3',
+    'Data_Vox_based_tMIP',
 ]
 
-# Initialize empty lists to store the data
-Data_radiologovia_nativ_nativ = []
-Data_radiologovia_nativ_faza1 = []
-Data_radiologovia_nativ_faza2 = []
-Data_radiologovia_nativ_faza3 = []
-Data_radiologovia_nativ_tMIP = []
-Data_vsetci_nativ = []
-Data_vsetci_faza1 = []
-Data_vsetci_faza2 = []
-Data_vsetci_faza3 = []
-Data_vsetci_tMIP = []
+# Function to drop highly correlated features from the data
+def drop_highly_correlated_features(data, threshold):
+    corr_matrix = data.corr().abs()
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+    # Find features with correlation greater than the threshold
+    to_drop = [column for column in upper.columns if any(upper[column] > threshold)]
+    # Drop highly correlated features
+    data.drop(to_drop, axis=1, inplace=True)
+    return data
 
-def getLabel(cestaKdatam):
-    # Read and transpose the label data from a CSV file
-    dataframe1 = np.transpose(pd.read_csv(cestaKdatam))
-    return dataframe1
-
-def remove_low_variance_features(data, threshold):
-    # Remove features with low variance from the data
-    data = np.asarray(data)
-    num_features = data.shape[1]
-    selected_features = []
-    
-    for feature_idx in range(num_features):
-        feature = data[:, feature_idx]
-        
-        # Calculate variance based on the data type
-        if np.issubdtype(feature.dtype, np.number):
-            variance = np.var(feature)
-        elif np.issubdtype(feature.dtype, np.unicode_) or np.issubdtype(feature.dtype, np.object_):
-            unique_values, counts = np.unique(feature, return_counts=True)
-            variance = np.var(counts)
-        else:
-            variance = 0
-        
-        if variance >= threshold:
-            selected_features.append(feature_idx)
-    
-    filtered_data = data[:, selected_features]
-    return filtered_data
-
+# Function to perform PCA on the data
 def perform_pca(data, num_components):
-    # Perform PCA on the data
     pca = PCA(n_components=num_components)
     transformed_data = pca.fit_transform(data)
-    return transformed_data
+    # Calculate the explained variance ratio
+    explained_variance_ratio = pca.explained_variance_ratio_
+    # Calculate the cumulative explained variance ratio
+    explained_variance_ratio_cumulative = np.cumsum(explained_variance_ratio)
+    return transformed_data, explained_variance_ratio_cumulative
 
-def compute_column_correlations(data):
-    # Compute the column-wise correlations of the data
-    correlations = np.corrcoef(data, rowvar=False)
-    return correlations
-
-def remove_non_numeric_values(data):
-    # Replace non-numeric values in the data with appropriate numeric representations
-    data = np.asarray(data)
-    num_rows, num_columns = data.shape
-    filtered_data = np.zeros_like(data)
-    
-    for column_idx in range(num_columns):
-        column = data[:, column_idx]
-        
-        # Check if the column contains non-numeric values
-        if not np.issubdtype(column.dtype, np.number):
-            unique_values, counts = np.unique(column, return_counts=True)
-            non_numeric_values = unique_values[np.logical_not(np.issubdtype(unique_values.dtype, np.number))]
-            
-            # Replace non-numeric values with appropriate numeric representations
-            for non_numeric_value in non_numeric_values:
-                numeric_value = np.argmax(unique_values == non_numeric_value)
-                column[column == non_numeric_value] = numeric_value
-        
-        filtered_data[:, column_idx] = column
-    
-    return filtered_data
-
-def Get_Only_Heterogeneity_Features(feature_map):
-    # Remove shape features from the feature map
-    features_to_drop = [i for i in range(feature_map.shape[1]) if 'shape' in str(i)]
-    filtered_feature_map = np.delete(feature_map, features_to_drop, axis=1)
-    return filtered_feature_map.astype(float)
-
+# Function to perform t-SNE on the data
 def perform_tsne(data, num_components):
-    # Perform t-SNE on the data
-    perplexity = min(5, data.shape[0] - 1)  # Set perplexity to a value lower than the number of samples
+    perplexity = 50  # Set perplexity to a value lower than the number of samples
     tsne = TSNE(n_components=num_components, perplexity=perplexity)
     transformed_data = tsne.fit_transform(data)
     return transformed_data
 
-def perform_kmeans(data, num_clusters):
-    # Perform k-means clustering on the data
-    kmeans = KMeans(n_clusters=num_clusters)
-    labels = kmeans.fit_predict(data)
-    return labels
+# Function to read label data from a CSV file
+def get_label(cestaKdatam):
+    # Read and transpose the label data from a CSV file
+    dataframe1 = np.transpose(pd.read_csv(cestaKdatam))
+    return dataframe1
 
+# Initialize empty lists for storing data
+Data_Vox_based_nativ = []
+Data_Vox_based_faza1 = []
+Data_Vox_based_faza2 = []
+Data_Vox_based_faza3 = []
+Data_Vox_based_tMIP = []
+
+IDs = []
 
 # Iterate over each directory and file to read the data
-for poradie in range(10):
+for poradie in range(5):
     for subdir, dirs, files in os.walk(Cesty[poradie]):
         for file in files:
             typsuboru = os.path.splitext(subdir + os.sep + file)[1]
@@ -141,68 +78,129 @@ for poradie in range(10):
                 cestaKlabelu = base + ".csv"
                 if os.path.isfile(cestaKlabelu):
                     print(cestaKlabelu)
-                    OneDatoLabel = getLabel(cestaKlabelu)
+                    IDs.append(base)
+                    OneDatoLabel = get_label(cestaKlabelu)
                     eval(Data[poradie]).append(OneDatoLabel)
 
-    np.save(Cesty[poradie] + ".npy", eval(Data[poradie]))
+data = eval(Data[3])
+df = pd.DataFrame()
+for i in range(len(data)):
+    obj_df = pd.DataFrame(data[i])
+    obj_df.columns = [f'Attribute_{i+1}_{j+1}' for j in range(obj_df.shape[1])]
+    df = pd.concat([df, obj_df], axis=1)
 
-# Perform analysis on Data_radiologovia_nativ_faza3
-data = np.array(eval(Data[3]))
-data = data[:, :, 0]
-
-# Remove low variance features
-kapa = Get_Only_Heterogeneity_Features(remove_low_variance_features(data, 0.01))
+# Remove NaN values from the DataFrame
+df = pd.DataFrame(df).T
+df = df.dropna()
 
 # Perform PCA
-pca_data = perform_pca(kapa, num_components=2)
+PCA_comp, explained_variance_ratio_cumulative = perform_pca(df, 5)
 
-# Remove non-numeric values
-preprocessed_data = remove_non_numeric_values(kapa)
+# Perform t-SNE on the data
+tsne_data = perform_tsne(df, num_components=2)
 
-# Compute column correlations
-correlations = compute_column_correlations(preprocessed_data.astype(float))
+# Perform UMAP on the data
+umap_data = umap.UMAP(n_components=2, n_neighbors=30, min_dist=0.1, metric='euclidean', random_state=42).fit_transform(df)
 
-# Perform t-SNE
-tsne_data = perform_tsne(kapa, num_components=3)
+droped_data=drop_highly_correlated_features(df, 0.7)
 
-# Perform k-means clustering
-num_clusters = 3  # Set the desired number of clusters
-kmeans_labels = perform_kmeans(kapa, num_clusters)
 
-# Plot the original data
+# Perform K-means clustering
+kmeans = KMeans(n_clusters=5)
+clusters = kmeans.fit_predict(PCA_comp)
+
+# Create a color map with 10 distinct colors
+color_map = plt.cm.get_cmap('tab10', 10)
+
+# Get the unique colors for each item
+num_items = len(data)
+item_colors = [color_map(i % 10) for i in range(num_items)]
+
+# Get the cluster labels for each item
+cluster_labels = clusters[:num_items]
+
+# Create a color map with unique background colors based on the cluster labels
+background_colors = [color_map(i % 10) for i in cluster_labels]
+
+# Plot the t-SNE-transformed data with colors based on the item and background colors based on the cluster labels
 plt.figure(figsize=(8, 6))
-plt.scatter(data[:, 0], data[:, 1])
-plt.title('Original Data')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
+for i, data_file in enumerate(data):
+    ID = IDs[i]
+    num_points = len(data_file)
+    item_color = item_colors[i]
+    background_color = background_colors[i]
+    tsne_data_file = tsne_data[i: i + num_points]
+    plt.scatter(tsne_data_file[:, 0], tsne_data_file[:, 1], c=item_color, label=f'File {i+1}', edgecolor=background_color)
+
+plt.title('t-SNE Transformed Data with K-means Clustering')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.legend()
 plt.show()
 
-# Plot the PCA-transformed data
+# Create a color map with 5 distinct colors based on the cluster labels
+cluster_colors = [color_map(i % 10) for i in clusters]
+
+# Plot the t-SNE-transformed data with colors based on the cluster labels
 plt.figure(figsize=(8, 6))
-plt.scatter(pca_data[:, 0], pca_data[:, 1])
-plt.title('PCA-Transformed Data')
+plt.scatter(tsne_data[:, 0], tsne_data[:, 1], c=cluster_colors)
+plt.title('t-SNE Transformed Data with K-means Clustering')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.show()
+
+# Plot the PCA-transformed data with colors based on the item and background colors based on the cluster labels
+plt.figure(figsize=(8, 6))
+for i, data_file in enumerate(data):
+    ID = IDs[i]
+    num_points = len(data_file)
+    item_color = item_colors[i]
+    background_color = background_colors[i]
+    PCA_comp_file = PCA_comp[i: i + num_points]
+    plt.scatter(PCA_comp_file[:, 0], PCA_comp_file[:, 1], c=item_color, label=f'File {i+1}', edgecolor=background_color)
+
+plt.title('PCA-Transformed Data with K-means Clustering')
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend()
+plt.show()
+
+# Plot the PCA-transformed data with colors based on the cluster labels
+plt.figure(figsize=(8, 6))
+plt.scatter(PCA_comp[:, 0], PCA_comp[:, 1], c=cluster_colors)
+plt.title('PCA-Transformed Data with K-means Clustering')
 plt.xlabel('PC 1')
 plt.ylabel('PC 2')
 plt.show()
 
-# Plot the correlation matrix
+# Plot the UMAP-transformed data with colors based on the item and background colors based on the cluster labels
 plt.figure(figsize=(8, 6))
-sns.heatmap(correlations, cmap='coolwarm', annot=True, fmt=".2f", square=True)
-plt.title('Correlation Matrix')
+for i, data_file in enumerate(data):
+    ID = IDs[i]
+    num_points = len(data_file)
+    item_color = item_colors[i]
+    background_color = background_colors[i]
+    umap_data_file = umap_data[i: i + num_points]
+    plt.scatter(umap_data_file[:, 0], umap_data_file[:, 1], c=item_color, label=f'File {i+1}', edgecolor=background_color)
+
+plt.title('UMAP-Transformed Data with K-means Clustering')
+plt.xlabel('UMAP 1')
+plt.ylabel('UMAP 2')
+plt.legend()
 plt.show()
 
-# Plot the t-SNE-transformed data
+# Plot the UMAP-transformed data with colors based on the cluster labels
 plt.figure(figsize=(8, 6))
-plt.scatter(tsne_data[:, 0], tsne_data[:, 1])
-plt.title('t-SNE Transformed Data')
-plt.xlabel('Dimension 1')
-plt.ylabel('Dimension 2')
+plt.scatter(umap_data[:, 0], umap_data[:, 1], c=cluster_colors)
+plt.title('UMAP-Transformed Data with K-means Clustering')
+plt.xlabel('UMAP 1')
+plt.ylabel('UMAP 2')
 plt.show()
 
-# Plot the k-means clustering results
+# Plot the raw data with colors based on the cluster labels
 plt.figure(figsize=(8, 6))
-plt.scatter(tsne_data[:, 0], tsne_data[:, 1], c=kmeans_labels)
-plt.title('k-means Clustering')
-plt.xlabel('Dimension 1')
-plt.ylabel('Dimension 2')
+plt.scatter(df.iloc[:, 0], df.iloc[:, 1], c=cluster_colors)
+plt.title('Raw Data with K-means Clustering (After Feature Selection)')
+plt.xlabel('Attribute 1')
+plt.ylabel('Attribute 2')
 plt.show()
